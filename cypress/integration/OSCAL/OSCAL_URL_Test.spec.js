84673a165de7a921baa2f3ad0e5a16ff65a08557
Sepideh Norouzzadeh <snorouzzadeh@easydynamics.com>
2021-10-04 14:15:15 -0400

    Improve user test experience Use text instead of HTML IDs for better user viewing the page experience

--- cypress/integration/OSCAL/OSCAL_URL_Test.spec.js
+++ cypress/integration/OSCAL/OSCAL_URL_Test.spec.js
@@ -1,38 +1,35 @@
-
 describe('system-security-plans', () => {
   it('successfully loads ssps ', () => {
     cy.visit('http://localhost:8080') 
-    cy.screenshot()
-    cy.get('.jss2 > .MuiIconButton-label > .MuiSvgIcon-root').click()
-    cy.get('.MuiList-root > :nth-child(2) > .MuiTypography-root').click()
-    cy.contains('OSCAL System Security Plan Viewer').should('be.visible')
-    cy.get('#oscal-url').should("be.enabled").type('{backspace}').clear()
-    cy.get('#oscal-url').type("http://localhost:8080/oscal/v1/ssps/ssp-example")
-    cy.screenshot()
-    cy.get('.MuiGrid-grid-xs-2 > .MuiButtonBase-root > .MuiButton-label').click()
+    cy.findByText('OSCAL Catalog Viewer').should('exist')
+    cy.get('button').first().click()
+    cy.contains('System Security Plan Viewer').click()
+    cy.findByText('OSCAL System Security Plan Viewer').should('be.visible')
+    cy.contains('OSCAL SSP URL').first().should('exist').next().click().clear().type("http://localhost:8080/oscal/v1/ssps/ssp-example")
+    cy.contains('Reload').click()
     cy.contains('Enterprise Logging and Auditing System Security Plan').should('be.visible')
-    cy.contains('Parties').should('be.visible')
-    cy.get(':nth-child(2) > .MuiPaper-root > .MuiList-root > :nth-child(2)')
-    cy.contains('This is a partial implementation that addresses the logging server portion of the auditing system.').should('be.visible')
-    cy.contains('Enterprise Logging, Monitoring, and Alerting Policy').should('be.visible')
-  })
+    cy.scrollTo('bottom')
+    cy.contains('This is the control implementation for the system.').should('be.visible')
 
+
+  })
 })
+    
 
 describe('component definitions', () => {
-  it('successfully loads components ', () => {
-    cy.visit('http://localhost:8080') 
-    cy.screenshot()
-    cy.get('.jss2 > .MuiIconButton-label > .MuiSvgIcon-root').click()
-    cy.get('.MuiList-root > :nth-child(3) > .MuiTypography-root').click()
-    cy.contains('OSCAL Component Viewer').should('be.visible')
-    cy.get('#oscal-url').should("be.enabled").type('{backspace}').clear()
-    cy.get('#oscal-url').type("http://localhost:8080/oscal/v1/components/example-component")
-    cy.screenshot()
-    cy.get('.MuiGrid-grid-xs-2 > .MuiButtonBase-root > .MuiButton-label').click()
-    cy.contains('Test Component Definition').should('be.visible')
-    cy.contains('Control Implementations').should('be.visible')
-    
+    it('successfully loads components ', () => {
+      cy.visit('http://localhost:8080') 
+      cy.findByText('OSCAL Catalog Viewer').should('exist')
+      cy.get('button').first().click()
+      cy.contains('Component Viewer').click()
+      cy.contains('OSCAL Component Viewer').should('be.visible')
+      cy.contains('Test Component Definition').should('be.visible')
+      cy.contains('OSCAL Component URL').first().should('exist').next().click().clear().type("http://localhost:8080/oscal/v1/components/example-component")
+      cy.contains('Reload').click()
+      cy.contains('Test Vendor').should('be.visible')
+      cy.scrollTo('bottom')
+
   })
 
 })
+
