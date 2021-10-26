b5485aa69bb81f82f877762259c9b2ad93c2fa0d
Michael Eisenberg <meisenberg@easydynamics.com>
2021-10-26 12:13:55 -0400

    Create base URL configuration
    Create the base localhost url used in the tests as an
    environment variable to allow for ovverriding when
    necessary.

--- end-to-end-tests/cypress/integration/OSCAL/OSCAL_URL_Test.spec.js
+++ end-to-end-tests/cypress/integration/OSCAL/OSCAL_URL_Test.spec.js
@@ -1,11 +1,11 @@
 describe('system-security-plans', () => {
   it('successfully loads ssps ', () => {
-    cy.visit('http://localhost:8080') 
+    cy.visit(Cypress.env('base_url')) 
     cy.findByText('OSCAL Catalog Viewer').should('exist')
     cy.get('button').first().click()
     cy.contains('System Security Plan Viewer').click()
     cy.findByText('OSCAL System Security Plan Viewer').should('be.visible')
-    cy.contains('OSCAL SSP URL').first().should('exist').next().click().clear().type("http://localhost:8080/oscal/v1/ssps/cff8385f-108e-40a5-8f7a-82f3dc0eaba8")
+    cy.contains('OSCAL SSP URL').first().should('exist').next().click().clear().type(Cypress.env('base_url') + "/oscal/v1/ssps/cff8385f-108e-40a5-8f7a-82f3dc0eaba8")
     cy.contains('Reload').click()
     cy.contains('Enterprise Logging and Auditing System Security Plan').should('be.visible')
     cy.scrollTo('bottom')
@@ -16,13 +16,13 @@ describe('system-security-plans', () => {
     
 describe('component definitions', () => {
     it('successfully loads components ', () => {
-      cy.visit('http://localhost:8080') 
+      cy.visit(Cypress.env('base_url')) 
       cy.findByText('OSCAL Catalog Viewer').should('exist')
       cy.get('button').first().click()
       cy.contains('Component Viewer').click()
       cy.contains('OSCAL Component Viewer').should('be.visible')
       cy.contains('Test Component Definition').should('be.visible')
-      cy.contains('OSCAL Component URL').first().should('exist').next().click().clear().type("http://localhost:8080/oscal/v1/component-definitions/8223d65f-57a9-4689-8f06-2a975ae2ad72")
+      cy.contains('OSCAL Component URL').first().should('exist').next().click().clear().type(Cypress.env('base_url') + "/oscal/v1/component-definitions/8223d65f-57a9-4689-8f06-2a975ae2ad72")
       cy.contains('Reload').click()
       cy.contains('Test Vendor').should('be.visible')
       cy.scrollTo('bottom')
