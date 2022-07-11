76c03d3de7f1e2af2d9ad03859544ded37eaae8c
Kyle Laker <klaker@easydynamics.com>
2022-07-11 17:46:32 -0400

    Bump timeout again

--- end-to-end-tests/cypress.config.js
+++ end-to-end-tests/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     base_url: 'http://localhost:8080',
     api_url: 'http://localhost:8080/oscal/v1',
   },
-  defaultCommandTimeout: 15000,
+  defaultCommandTimeout: 30000,
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {},
