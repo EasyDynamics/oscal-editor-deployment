b985353b2ae0acdac7c2ceacd5b1909025339c57
schen2166 <schen@easydynamics.com>
2022-08-01 14:27:27 -0400

    Revert code to forEach()
    After dicussion we concluded that using a forEach() is the
    better solution for testing navigation by URL.
    
    Cypress does not support it.each without adding additional
    dependencies. This should complete this issue

--- end-to-end-tests/cypress/e2e/OSCAL/url_navigation.cy.js
+++ end-to-end-tests/cypress/e2e/OSCAL/url_navigation.cy.js
@@ -41,9 +41,11 @@ const NAVIGATION = [
   },
 ];
 describe("Test that the ", () => {
-  it.each(NAVIGATION)(`%s loads by url`, (editor, title, relativeURL) => {
-    cy.visit(`${Cypress.env("base_url")}${relativeURL}`);
-    cy.waitForLoad();
-    cy.contains(`${title}`);
+  NAVIGATION.forEach((element) => {
+    it(`${element.editor} Editor loads by URL`, () => {
+      cy.visit(`${Cypress.env("base_url")}${element.relativeURL}`);
+      cy.waitForLoad();
+      cy.contains(element.title);
+    });
   });
 });
