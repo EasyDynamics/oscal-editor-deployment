8ab449ae11250d77ab0e243581a3aa907d79d9fd
Zachary Tucker <ztucker@easydynamics.com>
2023-03-23 10:01:16 -0400

    Catch ResizeObserver loop in e2e.js

--- end-to-end-tests/cypress/support/commands.js
+++ end-to-end-tests/cypress/support/commands.js
@@ -111,11 +111,3 @@ Cypress.Commands.add("setTestSspJson", (sspJson) => {
     sspJson
   );
 });
-
-const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
-Cypress.on('uncaught:exception', (err) => {
-    /* returning false here prevents Cypress from failing the test */
-    if (resizeObserverLoopErrRe.test(err.message)) {
-        return false
-    }
-})
