3fb8d241394f0d2cc5e6f28d806e36edd84aaeaa
Bradley Fellstrom <bfellstrom@easydynamics.com>
2023-04-14 17:22:47 -0400

    fix: Undo beforeEach()

--- end-to-end-tests/cypress/e2e/OSCAL/OSCAL_Load_Tests.cy.js
+++ end-to-end-tests/cypress/e2e/OSCAL/OSCAL_Load_Tests.cy.js
@@ -4,12 +4,9 @@ const CATALOG_TITLE =
   "NIST Special Publication 800-53 Revision 4: Security and Privacy Controls for Federal Information Systems and Organizations";
 
 describe("Loading system security plans", () => {
-  beforeEach(() => {
+  it("loads SSPs by REST Mode", () => {
     cy.navToSspEditor(SSP_TITLE_ORIG);
     cy.waitForLoad();
-  });
-
-  it("loads SSPs by REST Mode", () => {
     cy.get(`[aria-label="show code"]`).click();
     cy.scrollTo("bottom");
     cy.contains("This is the control implementation for the system.").should(
@@ -18,6 +15,8 @@ describe("Loading system security plans", () => {
   });
 
   it("loads system security plans by URL", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("REST Mode").click();
     cy.contains("OSCAL System Security Plan URL")
       .first()
@@ -38,6 +37,8 @@ describe("Loading system security plans", () => {
   });
 
   it("displays Enterprise Asset Owners Party", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Parties").click();
     cy.get(`[aria-label="Enterprise Asset Owners details button"]`).click();
     cy.contains("No address information provided");
@@ -46,6 +47,8 @@ describe("Loading system security plans", () => {
   });
 
   it("displays Enterprise Asset Administrators Party", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Parties").click();
     cy.get(
       `[aria-label="Enterprise Asset Administrators details button"]`
@@ -57,6 +60,8 @@ describe("Loading system security plans", () => {
   });
 
   it("navigates to roles from hash", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     const urlWithControlFragment =
       "http://localhost:8080/system-security-plans/cff8385f-108e-40a5-8f7a-82f3dc0eaba8#roles";
     cy.visit({
@@ -68,30 +73,40 @@ describe("Loading system security plans", () => {
   });
 
   it("navigates to the profile/catalog inheritance and grabs hash from anchor link", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Profiles/Catalog Inheritance").trigger("mouseover");
     cy.get(`[aria-label="profile-catalog-inheritance anchor link"]`).click();
     cy.url().should("include", "#profile-catalog-inheritance");
   });
 
   it("navigates to the system characteristics network architecture and grabs hash from anchor link", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Network Architecture").trigger("mouseover");
     cy.get(`[aria-label="network-architecture anchor link"]`).click();
     cy.url().should("include", "#network-architecture");
   });
 
   it("navigates to a system implementation inventory items and grabs hash from anchor link", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Inventory Items").trigger("mouseover");
     cy.get(`[aria-label="inventory-items anchor link"]`).click();
     cy.url().should("include", "#inventory-items");
   });
 
   it("navigates to the control implementation and grabs hash from anchor link", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Control Implementation").trigger("mouseover");
     cy.get(`[aria-label="control-implementation anchor link"]`).click();
     cy.url().should("include", "#control-implementation");
   });
 
   it("navigates to a control implementation sub-control (ac-2.5) and grabs hash from anchor link", () => {
+    cy.navToSspEditor(SSP_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("AC-2(5) Inactivity Logout").trigger("mouseover", {
       force: true,
     });
@@ -101,12 +116,9 @@ describe("Loading system security plans", () => {
 });
 
 describe("Loading Component Definitions", () => {
-  beforeEach(() => {
+  it("loads Components by REST Mode", () => {
     cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
     cy.waitForLoad();
-  });
-
-  it("loads Components by REST Mode", () => {
     cy.contains(COMP_DEF_TITLE_ORIG).should("be.visible");
     cy.contains("Parties").click();
     cy.contains("Test Vendor").should("be.visible");
@@ -115,6 +127,7 @@ describe("Loading Component Definitions", () => {
 
   it("loads components by URL", () => {
     cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains(COMP_DEF_TITLE_ORIG).should("be.visible");
     cy.contains("Parties").click();
     cy.contains("Test Vendor").should("be.visible");
@@ -122,6 +135,8 @@ describe("Loading Component Definitions", () => {
   });
 
   it("loads components by URL", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("REST Mode").click();
     cy.contains("OSCAL Component URL")
       .first()
@@ -141,24 +156,32 @@ describe("Loading Component Definitions", () => {
   });
 
   it("loads metadata roles", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Roles").click();
     cy.contains("Document creator");
     cy.contains("Contact");
   });
 
   it("loads metadata role details", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Roles").click();
     cy.get(`[aria-label="Document creator details button"]`).click();
     cy.contains("Creates documents describing the system");
   });
 
   it("loads metadata locations", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Locations").click();
     cy.contains("NIST");
     cy.contains("Not Specified");
   });
 
   it("loads metadata location details", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Locations").click();
     cy.get(`[aria-label="NIST details button"]`).click();
     cy.contains("100 Bureau Drive");
@@ -170,6 +193,8 @@ describe("Loading Component Definitions", () => {
   });
 
   it("navigates to parties from hash", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     const urlWithControlFragment =
       "http://localhost:8080/catalog/b954d3b7-d2c7-453b-8eb2-459e8d3b8462#parties";
     cy.visit({
@@ -183,12 +208,16 @@ describe("Loading Component Definitions", () => {
   });
 
   it("navigates to the control implementation and grabs hash from anchor link", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.contains("Control Implementations").trigger("mouseover");
     cy.get(`[aria-label="control-implementations anchor link"]`).click();
     cy.url().should("include", "#control-implementations");
   });
 
   it("navigates to a control implementation sub-control (ac-2.3) and grabs hash from anchor link", () => {
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
     cy.get("h2")
       .contains("AC-2(3) Disable Inactive Accounts")
       .first()
@@ -199,7 +228,7 @@ describe("Loading Component Definitions", () => {
 });
 
 describe("Errors caused by loading a bad component definition", () => {
-  beforeEach(() => {
+  it('display "yikes" on load of wrong object type', () => {
     sspExampleUrl =
       "https://raw.githubusercontent.com/usnistgov/oscal-content/main/examples/ssp/json/ssp-example.json";
     cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
@@ -213,13 +242,23 @@ describe("Errors caused by loading a bad component definition", () => {
       .clear()
       .type(sspExampleUrl);
     cy.contains("Reload").click();
-  });
-
-  it('display "yikes" on load of wrong object type', () => {
     cy.contains("Yikes").should("be.visible");
   });
 
   it("do not persist after loading a valid component in Viewer", () => {
+    sspExampleUrl =
+      "https://raw.githubusercontent.com/usnistgov/oscal-content/main/examples/ssp/json/ssp-example.json";
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Component URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(sspExampleUrl);
+    cy.contains("Reload").click();
     const exampleCompDefUrl =
       "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/component-definitions/example-component.json";
     cy.contains("OSCAL Component URL")
@@ -234,13 +273,26 @@ describe("Errors caused by loading a bad component definition", () => {
   });
 
   it("do not persist after loading a valid component in Editor", () => {
+    sspExampleUrl =
+      "https://raw.githubusercontent.com/usnistgov/oscal-content/main/examples/ssp/json/ssp-example.json";
+    cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Component URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(sspExampleUrl);
+    cy.contains("Reload").click();
     cy.contains("REST Mode").click();
     cy.navToCdefEditor(COMP_DEF_TITLE_ORIG);
   });
 });
 
 describe("Loading OSCAL Catalog Groups", () => {
-  beforeEach(() => {
+  it("navigates to parties section from hash", () => {
     const exampleCatalogUrl =
       "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/catalogs/NIST_SP-800-53_rev4_catalog.json";
     cy.navToCatalogEditor(CATALOG_TITLE);
@@ -255,9 +307,6 @@ describe("Loading OSCAL Catalog Groups", () => {
       .type(exampleCatalogUrl);
     cy.contains("Reload").click();
     cy.contains("Control Groups").should("be.visible");
-  });
-
-  it("navigates to parties section from hash", () => {
     const urlWithControlFragment =
       "http://localhost:8080/catalog/b954d3b7-d2c7-453b-8eb2-459e8d3b8462#parties";
     cy.visit({
@@ -271,6 +320,19 @@ describe("Loading OSCAL Catalog Groups", () => {
   });
 
   it("navigates to roles from hash", () => {
+    const exampleCatalogUrl =
+      "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/catalogs/NIST_SP-800-53_rev4_catalog.json";
+    cy.navToCatalogEditor(CATALOG_TITLE);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Catalog URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(exampleCatalogUrl);
+    cy.contains("Reload").click();
     const urlWithControlFragment =
       "http://localhost:8080/catalog/b954d3b7-d2c7-453b-8eb2-459e8d3b8462#roles";
     cy.visit({
@@ -282,12 +344,38 @@ describe("Loading OSCAL Catalog Groups", () => {
   });
 
   it("displays a control (CP-2)", () => {
+    const exampleCatalogUrl =
+      "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/catalogs/NIST_SP-800-53_rev4_catalog.json";
+    cy.navToCatalogEditor(CATALOG_TITLE);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Catalog URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(exampleCatalogUrl);
+    cy.contains("Reload").click();
     cy.get("button").contains("Contingency Planning").click();
     cy.contains("CP-2 Contingency Plan").click();
     cy.contains("The organization:").should("be.visible");
   });
 
   it("navigates to top-level control (PM-11) from hash", () => {
+    const exampleCatalogUrl =
+      "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/catalogs/NIST_SP-800-53_rev4_catalog.json";
+    cy.navToCatalogEditor(CATALOG_TITLE);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Catalog URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(exampleCatalogUrl);
+    cy.contains("Reload").click();
     const urlWithControlFragment =
       "http://localhost:8080/catalog/b954d3b7-d2c7-453b-8eb2-459e8d3b8462#pm/pm-11";
     cy.visit({
@@ -299,6 +387,19 @@ describe("Loading OSCAL Catalog Groups", () => {
   });
 
   it("navigates to sub-level control (AU-2.2) from hash", () => {
+    const exampleCatalogUrl =
+      "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/catalogs/NIST_SP-800-53_rev4_catalog.json";
+    cy.navToCatalogEditor(CATALOG_TITLE);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Catalog URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(exampleCatalogUrl);
+    cy.contains("Reload").click();
     const urlWithControlFragment =
       "http://localhost:8080/catalog/b954d3b7-d2c7-453b-8eb2-459e8d3b8462#au/au-2/au-2.2";
     cy.visit({
@@ -312,6 +413,19 @@ describe("Loading OSCAL Catalog Groups", () => {
   });
 
   it("navigates to control (RA-5) and grabs hash from anchor link", () => {
+    const exampleCatalogUrl =
+      "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/catalogs/NIST_SP-800-53_rev4_catalog.json";
+    cy.navToCatalogEditor(CATALOG_TITLE);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Catalog URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(exampleCatalogUrl);
+    cy.contains("Reload").click();
     cy.get("button").contains("Risk Assessment").click();
     cy.contains("Vulnerability Scanning").trigger("mouseover");
     cy.get(`[aria-label="ra/ra-5 anchor link"]`).click();
@@ -319,6 +433,19 @@ describe("Loading OSCAL Catalog Groups", () => {
   });
 
   it("navigates to a backmatter resource (CNSS Policy 15) and grabs hash from anchor link", () => {
+    const exampleCatalogUrl =
+      "https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/main/catalogs/NIST_SP-800-53_rev4_catalog.json";
+    cy.navToCatalogEditor(CATALOG_TITLE);
+    cy.waitForLoad();
+    cy.contains("REST Mode").click();
+    cy.contains("OSCAL Catalog URL")
+      .first()
+      .should("exist")
+      .next()
+      .click()
+      .clear()
+      .type(exampleCatalogUrl);
+    cy.contains("Reload").click();
     cy.contains("CNSS Policy 15").trigger("mouseover");
     cy.get(
       `[aria-label="a4aa9645-9a8a-4b51-90a9-e223250f9a75 anchor link"]`
