0faa7f9c1ec90a4ec09b66a94f23fea80404dcb8
Bradley Fellstrom <bfellstrom@easydynamics.com>
2023-07-13 09:14:36 -0400

    Add disable no-explicit-any line

--- end-to-end-tests/cypress.config.ts
+++ end-to-end-tests/cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     api_url: "http://localhost:8080/oscal/v1",
   },
   chromeWebSecurity: false,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
   e2e: { setupNodeEvents(on: any, config: any) {} },
   retries: 1,
   video: false,
